882bf79348c2a11d98dbd22e43c258694a5d5930
kaunain2005 <kaunainshaikh84@gmail.com>
2025-10-16 15:03:19 +0530

    #Category Add - Backend

--- backend/actions/functions.php
+++ backend/actions/functions.php
@@ -6,10 +6,26 @@ if (isset($_POST['add-categories'])) {
     $meta_title = $_POST['meta_title'];
     $meta_key = $_POST['meta_key'];
     $meta_desc = $_POST['meta_desc'];
-    $status = $_POST['status'];
+    $slug_url = SlugUrl($cate_name);
     $added_on = date('M d, Y h:i:s A');
     $updated_on = date('M d, Y h:i:s A');
     echo $added_on;
 
-    $sql = "INSERT INTO fk_categories (cate_name, meta_title, meta_key, meta_desc, status, added_on, updated_on) VALUES ('$cate_name', '$meta_title', '$meta_key', '$meta_desc', '$status', '$added_on', '$updated_on')";
+    $sql = "INSERT INTO fk_categories (cate_id, cate_name, meta_title, meta_key, meta_desc, slug_url, status, added_on, updated_on) VALUES ('$cate_id', '$cate_name', '$meta_title', '$meta_key', '$meta_desc', '$slug_url', 1, '$added_on', '$updated_on')";
+
+    $check = mysqli_query($conn, $sql);
+    if ($check) {
+        
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+
+function SlugUrl($string)
+{
+    $slug = trim($string); // trim the string
+    $slug = preg_replace('/[^a-zA-Z0-9 -]/', '', $slug); // only take alphanumerical characters, but keep the spaces and dashes too ...
+    $slug = str_replace(' ', '-', $slug); // replace spaces by dashes
+    $slug = strtolower($slug); // make it lowercase
+    return $slug;
 }
